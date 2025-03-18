1551eb52799fa296369aad33b33f700573a733cf
Joao Pedro Frare <ra281260@beatles.lab.ic.unicamp.br>
2025-03-18 14:20:47 -0300

    atributos e metodos basicos de robo

--- lab02/Robo.java
+++ lab02/Robo.java
@@ -1,4 +1,4 @@
-
+package lab02;
 
 public abstract class Robo {
     private String nome;
@@ -6,6 +6,29 @@ public abstract class Robo {
     private int posicaoX;
     private int posicaoY;
 
-    
-    
+    void setPosicaoX(int posicaoX) {
+        this.posicaoX = posicaoX;
+    }
+
+    void setPosicaoY(int posicaoY) {
+        this.posicaoY = posicaoY;
+    }
+
+    int getPosicaoX() {
+        return this.posicaoX;
+    }
+
+    int getPosicaoY() {
+        return this.posicaoY;
+    }
+
+    void mover(int deltaX, int deltaY){
+        if ((this.getPosicaoX() + deltaX < 0) || (this.getPosicaoY() + deltaY < 0)) {
+            System.out.printf("Movimento Inválido! \n");
+            return;
+        }
+
+        
+    }
+
 }
