f435d3ea4ea48da559b354521820060df4e92d0d
Guilherme Jose de Oliveira Almeida <ra281218@beach.lab.ic.unicamp.br>
2025-03-18 15:43:40 -0300

    implementacao dos atributos referentes a posicao do obstaculo e lista de robos do ambiente, bem como alteracao do construtor e implementacao de metodos para adicionar elementos a lista e verificar se posicao eh um obstaculo

---
+++ lab02/Ambiente.java
@@ -0,0 +1,35 @@
+package lab02;
+import java.util.ArrayList;
+
+public class Ambiente {
+    private final int X;
+    private final int Y;
+    private final int X_obstaculo;
+    private final int Y_obstaculo;
+    private final ArrayList<Robo> robos;
+
+    public Ambiente(int x, int y, int x_obstaculo, int y_obstaulo) {
+        //inicializa o Ambiente atribuindo valores as dimensoes x e y e cria um array vazio de robos
+        this.X = x;
+        this.Y = y;
+        this.X_obstaculo = x_obstaculo;
+        this.Y_obstaculo = y_obstaulo;
+        this.robos = new ArrayList<>();
+    }
+
+    public boolean eh_obstaculo(int x, int y){
+        //retorna true se as coordenadas x e y no parametro correspondem a localizacao x e y do obstaculo
+        return (x == X_obstaculo && y == Y_obstaculo);
+    }
+
+    public boolean dentroDosLimites(int x, int y) {
+        //retorna true se o robo esta dentro do ambiente e false caso contrario
+        return (x >= 0 && this.X >= x && this.Y >= y && y >= 0);
+    }
+
+    public void adicionaRobo(Robo robo){
+        //adiciona um objeto da classe robo ao array de robos
+        this.robos.add(robo);
+    }
+
+}
