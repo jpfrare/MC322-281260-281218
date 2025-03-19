6c86432033b2d8d7c6878e482c882471f6f5adf2
João Pedro Frare <281260@dac.unicamp.br>
2025-03-18 21:41:19 -0300

    teste de roboterrestre na main

--- lab02/RoboTerrestre.java
+++ lab02/RoboTerrestre.java
@@ -1,5 +1,6 @@
+
 public class RoboTerrestre extends Robo {
-    private int velocidademax;
+    private final int velocidademax;
 
     public RoboTerrestre(int posicaoXo, int posicaoYo, String nome, int velocidademax) {
         super(posicaoXo, posicaoYo, nome);
@@ -8,6 +9,14 @@ public class RoboTerrestre extends Robo {
 
     @Override
     void mover(int deltaX, int deltaY) {
-        
+        if ((Math.abs(deltaX) > this.velocidademax) || (Math.abs(deltaY) > this.velocidademax) ||
+        (this.getPosicaoX() + deltaX < 0) || (this.getPosicaoY() + deltaY < 0)) {
+
+            System.out.printf("Movimento Invalido!\n");
+            return;
+        }
+
+        this.setPosicaoX(this.getPosicaoX() + deltaX);
+        this.setPosicaoY(this.getPosicaoY() + deltaY);
     }
 }
