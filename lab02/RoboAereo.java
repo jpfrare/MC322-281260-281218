908d765966e22814d081dca533e47d03db476daa
guialmeidajose <guialmeidajose@gmail.com>
2025-03-24 18:19:47 -0300

    implementacao do construtor da classe RoboAereo

--- lab02/RoboAereo.java
+++ lab02/RoboAereo.java
@@ -1,5 +1,11 @@
 public class RoboAereo extends Robo {
-    private int altitude;
-    private int altitudemax;
+    private final int altitude;
+    private final int altitudeMax;
+
+    public RoboAereo(int posXo, int posYo, int alt_o, int alt_max, String nome){
+        super(posXo, posYo, nome);
+        this.altitude = alt_o;
+        this.altitudeMax = alt_max;
+    }
 
 }
