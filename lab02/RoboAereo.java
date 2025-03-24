ca0621f27da2ba89e90b9ca2dfd0bae35da27cdd
guialmeidajose <guialmeidajose@gmail.com>
2025-03-24 18:25:49 -0300

    implementacao do metodo descer da classe RoboAereo

--- lab02/RoboAereo.java
+++ lab02/RoboAereo.java
@@ -1,11 +1,29 @@
 public class RoboAereo extends Robo {
-    private final int altitude;
+    private int altitude;
     private final int altitudeMax;
 
     public RoboAereo(int posXo, int posYo, int alt_o, int alt_max, String nome){
         super(posXo, posYo, nome);
         this.altitude = alt_o;
         this.altitudeMax = alt_max;
+    }
+
+    void subir(int delta_h){
+        if(this.altitude + delta_h <= this.altitudeMax){
+            this.altitude += delta_h;
+        }
+        else{
+            this.altitude = this.altitudeMax;
+        }
+    }
+
+    void descer(int delta_h){
+        if(this.altitude - delta_h >= 0){
+            this.altitude -= delta_h;
+        }
+        else{
+            this.altitude = 0;
+        }
     }
 
 }
