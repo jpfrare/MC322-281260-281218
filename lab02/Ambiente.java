1babd865521c56d5183eef8408f6064f13fc7bb8
Guilherme Jose de Oliveira Almeida <ra281218@deep.lab.ic.unicamp.br>
2025-03-25 15:43:14 -0300

    adaptacao de atributos e metodos da classe ambiente para o espaco tridimensional

--- lab02/Ambiente.java
+++ lab02/Ambiente.java
@@ -3,22 +3,24 @@ import java.util.ArrayList;
 public class Ambiente {
     private final int X;
     private final int Y;
-    private final int X_obstaculo;
-    private final int Y_obstaculo;
     public final ArrayList<Robo> robos;
 
-    public Ambiente(int x, int y, int x_obstaculo, int y_obstaulo) {
+    public Ambiente(int x, int y) {
         //inicializa o Ambiente atribuindo valores as dimensoes x e y e cria um array vazio de robos
         this.X = x;
         this.Y = y;
-        this.X_obstaculo = x_obstaculo;
-        this.Y_obstaculo = y_obstaulo;
         this.robos = new ArrayList<>();
     }
 
-    public boolean eh_obstaculo(int x, int y){
-        //retorna true se as coordenadas x e y no parametro correspondem a localizacao x e y do obstaculo
-        return (x == X_obstaculo && y == Y_obstaculo);
+    public boolean eh_obstaculo(ArrayList<Robo> robos, int x, int y){
+        //retorna true se as coordenadas x e y no parametro correspondem a localizacao x e y de um obstaculo (tradado como robo)
+        int i;
+        for(i = 0; i < robos.size(); i++){
+            if(robos.get(i).getPosicaoX() == x && robos.get(i).getPosicaoY() == y){
+                return true;
+            }
+        }
+        return false;
     }
 
     public boolean dentroDosLimites(int x, int y, int z) {
