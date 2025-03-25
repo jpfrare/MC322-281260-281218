50486a8e5577c75ab1aaa241054468267685d917
Joao Pedro Frare <ra281260@purple.lab.ic.unicamp.br>
2025-03-25 15:04:24 -0300

    alterações na classe abstrata

--- lab02/Robo.java
+++ lab02/Robo.java
@@ -5,25 +5,40 @@ public abstract class Robo {
     private int posicaoY;
 
     public Robo (int posicaoXo, int posicaoYo, String nome) {
+        //construtor padrão
         this.posicaoX = posicaoXo;
         this.posicaoY = posicaoYo;
         this.nome = nome;
     }
 
     void setPosicaoX(int posicaoX) {
+        //mudar a posição em X
         this.posicaoX = posicaoX;
     }
 
     void setPosicaoY(int posicaoY) {
+        //mudar a posição em Y
         this.posicaoY = posicaoY;
     }
 
     int getPosicaoX() {
+        //retorna a posicão em X
         return this.posicaoX;
     }
 
     int getPosicaoY() {
+        //retorna a posição em Y
         return this.posicaoY;
+    }
+
+    void setDirecao(String direcao){
+        //muda a direção do robô
+        this.direcao = direcao;
+    }
+
+    String getDirecao() {
+        //retorna a direção do robô
+        return this.direcao;
     }
 
     void mover(int deltaX, int deltaY){
@@ -41,5 +56,7 @@ public abstract class Robo {
     }
 
 
-    //identificarObstaculo() criar!!
+    void identificarObstaculo(Ambiente a) {
+        
+    }
 }
