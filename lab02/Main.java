23c9e595d60b3006335f28792d86e03603cf1c63
guialmeidajose <guialmeidajose@gmail.com>
2025-03-29 12:21:25 -0300

    main

--- lab02/Main.java
+++ lab02/Main.java
@@ -18,6 +18,9 @@ public class Main {
         dinamico_1.exibirPosicao();
         dinamico_1.mover(5,-2,1, simulador_1);
         dinamico_1.exibirPosicao();
+        dinamico_1.mover(5,-2,1, simulador_1);
+        dinamico_1.exibirPosicao();
+
         System.out.println("");
         dinamico_2.exibirPosicao();
         dinamico_3.exibirPosicao();
