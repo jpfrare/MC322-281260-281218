16b68910a0ccddc1dbad032b2e9adf41012e7bc3
Guilherme Jose de Oliveira Almeida <ra281218@cult.lab.ic.unicamp.br>
2025-04-15 15:08:47 -0300

    mudanca no enum TipoObstaculo para impedimento ou nao de passagem

--- lab03/TipoObstaculo.java
+++ lab03/TipoObstaculo.java
@@ -1,15 +1,30 @@
 public enum TipoObstaculo {
-    MURO(10), 
-    BLOCO(1),
-    PLACA(2);
+    MURO(10, true), 
+    BLOCO(1, true),
+    PLACA(2, false);
 
     private final int altura; //dimensao da altura;
+    private final boolean bloqueia;
 
-    TipoObstaculo(int altura) {
+    TipoObstaculo(int altura, boolean bloqueia) {
         this.altura = altura;
+        this.bloqueia = bloqueia;
     }
 
     int getAltura() {
         return this.altura;
+    }
+
+    boolean getBloqueia(){
+        return this.bloqueia;
+    }
+
+    boolean impede_passagem(int h){
+        for(TipoObstaculo obstaculo: TipoObstaculo.values()){
+            if(obstaculo.getAltura() == h && !obstaculo.bloqueia){
+                return false;
+            }
+        }
+        return true;
     }
 }
