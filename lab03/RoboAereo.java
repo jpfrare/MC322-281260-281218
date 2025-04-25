05b76e87ca87527eda6888230c47ad5390d4c26a
guialmeidajose <guialmeidajose@gmail.com>
2025-04-25 13:28:25 -0300

    mudancas em mover do Robo

--- lab03/RoboAereo.java
+++ lab03/RoboAereo.java
@@ -59,73 +59,7 @@ public class RoboAereo extends Robo {
             }
         }
     }
-/* 
-    @Override
-    boolean moverR(int deltaX, int deltaY, int passoX, int passoY, int [][] visitados){
-        visitados[passoX][passoY] = 1;
-        int avancar;
-        if(deltaX == 0 && deltaY == 0){ //chegou ao destino
-            return true;
-        }
-        for(int i = 1; i <= 2; i++){
-            //iteracao: i igual a 1 move em x, i igual a 2 move em y
-            if(i == 1){ //mover em x
-                if(deltaX > 0){
-                    //primeiro verifica se a andar +1 em x ira cair em uma posicao de obstaculo ou se essa posicao ja foi verificada
-                    avancar = this.getSensorMovimento().consegueAvancar(1, 1, this.getPosicaoX(), this.getPosicaoY(), this.getPosicaoZ(), deltaX, this.getAmbiente());
-                    while(avancar > 0){
-                        if(!this.getAmbiente().identifica_colisao(this.getPosicaoX() + avancar, this.getPosicaoY(), this.getPosicaoZ()) && visitados[passoX + avancar][passoY] == 0){
-                            this.setPosicaoX(this.getPosicaoX() + avancar);
-                            if(moverR(deltaX - avancar, deltaY, passoX + avancar, passoY, visitados))
-                                return true;
-                        }
-                        avancar--;
-                    }
-                }
-                else if(deltaX < 0){
-                    avancar = this.getSensorMovimento().consegueAvancar(1, 2, this.getPosicaoX(), this.getPosicaoY(), this.getPosicaoZ(), -deltaX, this.getAmbiente()); 
-                    //primeiro verifica se a andar -1 em x ira cair em uma posicao de obstaculo ou se essa posicao ja foi verificada
-                    while(avancar > 0){
-                        if(!this.getAmbiente().identifica_colisao(this.getPosicaoX() - avancar, this.getPosicaoY(), this.getPosicaoZ()) && visitados[passoX + avancar][passoY] == 0){
-                            this.setPosicaoX(this.getPosicaoX() - avancar);
-                            if(moverR(deltaX + avancar, deltaY, passoX + avancar, passoY, visitados))
-                                return true;
-                        }
-                        avancar --;
-                    }
-                        
-                }
-            }
-            if(i == 2){//mover em y
-                if(deltaY > 0){
-                    avancar = this.getSensorMovimento().consegueAvancar(2, 1, this.getPosicaoX(), this.getPosicaoY(), this.getPosicaoZ(), deltaY, this.getAmbiente()); 
-                    //primeiro verifica se a andar +1 em y ira cair em uma posicao de obstaculo ou se essa posicao ja foi verificada
-                    while(avancar > 0){
-                        if(!this.getAmbiente().identifica_colisao(this.getPosicaoX(), this.getPosicaoY() + avancar, this.getPosicaoZ()) && visitados[passoX][passoY + avancar] == 0){
-                            this.setPosicaoY(this.getPosicaoY() + avancar);
-                            if(moverR(deltaX, deltaY - avancar, passoX, passoY + avancar, visitados))
-                                return true;
-                        }
-                        avancar --;
-                    }
-                }
-                else if(deltaY < 0){
-                    avancar = this.getSensorMovimento().consegueAvancar(2, 2, this.getPosicaoX(), this.getPosicaoY(), this.getPosicaoZ(), -deltaY, this.getAmbiente()); 
-                    //primeiro verifica se a andar -1 em y ira cair em uma posicao de obstaculo ou se essa posicao ja foi verificada
-                    while(avancar > 0){
-                        if(!this.getAmbiente().identifica_colisao(this.getPosicaoX(), this.getPosicaoY() - avancar, this.getPosicaoZ()) && visitados[passoX][passoY + avancar] == 0){
-                            this.setPosicaoY(this.getPosicaoY() - avancar);
-                            if(moverR(deltaX, deltaY + avancar, passoX, passoY + avancar, visitados))
-                                return true;
-                        }
-                        avancar--;
-                    }
-                }
-            }
-        }
-        return false;
-    }
-*/
+    
     void exibirPosicao() {
         System.out.printf("Robo %s: \n r(x,y,z) = (%d, %d, %d)\n", this.getNome(), this.getPosicaoX(), this.getPosicaoY(), this.getPosicaoZ());
     }
