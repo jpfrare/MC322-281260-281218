f028a67b5f5ebe9e011def35c020513ec3c4dd01
guialmeidajose <guialmeidajose@gmail.com>
2025-05-24 20:27:34 -0300

    realizacao do mover para o ambiente

--- lab04/Ambiente.java
+++ lab04/Ambiente.java
@@ -37,29 +37,44 @@ public class Ambiente {
 
     }
 
-    public void registra_obstaculo(Obstaculo objeto){
-        for(int i = objeto.getX(); i <= objeto.getX2(); i++){
-            for(int j = objeto.getY(); j <= objeto.getY2(); j++){
-                for(int k = 0; k <= objeto.getZ(); k++){
-                    this.mapa[i][j][k] = TipoEntidade.OBSTACULO;
+    public boolean espaco_vazio(Obstaculo objeto){
+        int i, j;
+        for( i = objeto.getX(); i <= objeto.getX2(); i++){
+            for(j = objeto.getY(); j <= objeto.getY2(); j++){
+                for(int k = objeto.getZ(); k <= objeto.getZ2(); k++){
+                    if(this.identifica_colisao(i, j, k))
+                        return false;
                 }
             }
         }
-        
+        return true;
+    }
+
+    public void registra_obstaculo(Obstaculo objeto){
+        for(int i = objeto.getX(); i <= objeto.getX2(); i++){
+            for(int j = objeto.getY(); j <= objeto.getY2(); j++){
+                for(int k = objeto.getZ(); k <= objeto.getZ2(); k++){
+                    this.mapa[i][j][k] = TipoEntidade.OBSTACULO;
+                }
+            }
+        }        
     }
 
     public void apaga_obstaculo(Obstaculo objeto){
         for(int i = objeto.getX(); i <= objeto.getX2(); i++){
             for(int j = objeto.getY(); j <= objeto.getY2(); j++){
-                for(int k = 0; k <= objeto.getZ(); k++){
+                for(int k = objeto.getZ(); k <= objeto.getZ2(); k++){
                     this.mapa[i][j][k] = TipoEntidade.VAZIO;
                 }
             }
         }
     }
 
-    public void registra_robo(InterfaceEntidade robo){
+    public boolean registra_robo(InterfaceEntidade robo){
+        if(this.identifica_colisao(robo.getX(), robo.getY(), robo.getZ()))
+            return false;
         this.mapa[robo.getX()][robo.getY()][robo.getZ()] = TipoEntidade.ROBO;
+        return true;
     }
 
     public void apaga_robo(InterfaceEntidade robo){
@@ -74,13 +89,15 @@ public class Ambiente {
 
     public void adicionarEntidade(InterfaceEntidade adicionar){
         if(adicionar.getTipo() == TipoEntidade.ROBO){
-            this.registra_robo(adicionar);
+            if(this.registra_robo(adicionar))
+                this.elementos.add((Robo)adicionar);
         }
         else if(adicionar.getTipo() == TipoEntidade.OBSTACULO){
-            this.registra_obstaculo((Obstaculo)adicionar);
+            if(this.espaco_vazio((Obstaculo)adicionar)){
+                this.registra_obstaculo((Obstaculo)adicionar);
+                this.elementos.add((Obstaculo)adicionar);
+            }
         }
-        this.elementos.add(adicionar);
-
     }
 
     public void removerEntidade(InterfaceEntidade remover){
@@ -91,6 +108,59 @@ public class Ambiente {
             this.apaga_obstaculo((Obstaculo)(InterfaceEntidadeObstaculo)remover);
         }
         elementos.remove(remover);
+    }
+
+    public void moverEntidade(InterfaceEntidade mover, int novoX, int novoY, int novoZ){
+        //funcao que altera as posicoes dos elementos que compoem o ambiente
+        //não se trata de um mover convencional(uma vez que não é realizado um movimento em si, apenas alteração da posicao)
+        //seria como se quem estivesse executando o simulador de robos realizasse manualmente a mudanca de posicao.
+        if(!dentroDosLimites(novoX, novoY, novoZ) || this.identifica_colisao(novoX, novoY, novoZ)){
+
+        }
+        else{
+            if(mover.getTipo() == TipoEntidade.ROBO){
+                apaga_robo(mover);
+                Robo r = (Robo)mover;
+                r.setPosicaoX(novoX);
+                r.setPosicaoY(novoY);
+                r.setPosicaoZ(novoZ);
+                this.mapa[novoX][novoY][novoZ] = TipoEntidade.ROBO;
+            }
+            if(mover.getTipo() == TipoEntidade.OBSTACULO){
+                InterfaceEntidadeObstaculo objeto = (InterfaceEntidadeObstaculo)mover;
+                Obstaculo obstaculo = (Obstaculo)objeto;
+                int x1_ini = obstaculo.getX();
+                int x2_ini = obstaculo.getX2();
+                int y1_ini = obstaculo.getY();
+                int y2_ini = obstaculo.getY2();
+                int z1_ini = obstaculo.getZ();
+                int z2_ini = obstaculo.getZ2();
+                int largx = x2_ini - x1_ini;
+                int largy = y2_ini - y1_ini;
+                int largz = z2_ini - z1_ini;
+                if(dentroDosLimites(novoX  + largx, novoY + largy, novoZ + largz) && dentroDosLimites(novoX, novoY, novoZ)){
+                    this.apaga_obstaculo(obstaculo);
+                    obstaculo.setX(novoX);
+                    obstaculo.setX2(novoX + largx);
+                    obstaculo.setY(novoY);
+                    obstaculo.setY2(novoY + largy);
+                    obstaculo.setZ(novoZ);
+                    obstaculo.setZ2(novoZ + largz);
+                    if(this.espaco_vazio(obstaculo))
+                        this.registra_obstaculo(obstaculo);
+                    else{
+                        obstaculo.setX(x1_ini);
+                        obstaculo.setX2(x2_ini);
+                        obstaculo.setY(y1_ini);
+                        obstaculo.setY2(y2_ini);
+                        obstaculo.setZ(z1_ini);
+                        obstaculo.setZ2(z2_ini);
+                        this.registra_obstaculo(obstaculo);
+                    }
+                
+                }
+            }
+        }
     }
 
 
