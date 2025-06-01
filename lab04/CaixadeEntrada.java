008da6161982fcdbee06623965f3572ba6c706f2
guialmeidajose <guialmeidajose@gmail.com>
2025-06-01 16:29:41 -0300

    diagrama de classes

--- lab04/CaixadeEntrada.java
+++ lab04/CaixadeEntrada.java
@@ -25,10 +25,4 @@ public class CaixadeEntrada {
             this.nao_lidas--;
         }
     }
-
-    public void esvaziar_caixa(){
-        while(this.nao_lidas > 0){
-            this.ler_mensagem();
-        }
-    }
 }
