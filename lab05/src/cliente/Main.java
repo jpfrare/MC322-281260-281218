d38c1a43fbca3a78dc576b31c3aedf1ab34d086c
João Pedro Frare <281260@dac.unicamp.br>
2025-06-13 18:42:50 -0300

    alteração na lógica de escrever arquivo

--- lab05/src/cliente/Main.java
+++ lab05/src/cliente/Main.java
@@ -45,7 +45,7 @@ public class Main {
                 amb.adicionarEntidade(agTerrestre);
                 //limpar o arquivo antes de iniciar o sistema de simulacao de robos
                 try {
-                        FileWriter escritor = new FileWriter("../../logMissao.txt");
+                        FileWriter escritor = new FileWriter("logMissao.txt");
                         escritor.write("");
                         escritor.close();
                 } catch (IOException e) {
@@ -304,7 +304,7 @@ public class Main {
                                 if(p != null){
                                         if(p instanceof AgenteInteligenteAereo || p instanceof AgenteInteligenteTerrestre){
                                                InterfaceRoboMissionario missionario = (InterfaceRoboMissionario)p;
-                                               missionario.executarMissao("../logMissao.txt"); 
+                                               missionario.executarMissao("logMissao.txt"); 
                                         }
                                         else
                                                 System.out.println("Robo selecionado nao executa missao.");
